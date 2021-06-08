80c0667264c2f560227fa2ad71f59c04563a78c0
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-08 13:08:33 +0900

    210608 guesswhat

--- guesswhat1/src/main/java/com/example/guesswhat1/MainActivity.java
+++ guesswhat1/src/main/java/com/example/guesswhat1/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int rnd, num, cnt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +19,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.guesswhat_layout1);
 
         Button ok = findViewById(R.id.button);
+
+        rnd = getRandomRange(10, 30);
+        TextView tv = findViewById(R.id.result_txt);
+
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double rnd = Math.random();
+                EditText ed = findViewById(R.id.editTextNumber);
+                String str = ed.getText().toString();
 
-                int int_rnd = (int)(rnd*22 + 1);
+                // 1. 입력 됐는지?
+                //  - no : 숫자 입력 요구
+                if ("".equals(str)) {
+                    Toast.makeText(MainActivity.this, "입력해 주세요", Toast.LENGTH_SHORT).show();
+                } else {
+                    // 2. 입력
+                    //  - rnd 비교
+                    String msg;
 
-                Toast.makeText(MainActivity.this, ""+int_rnd, Toast.LENGTH_SHORT).show();
+                    num = Integer.parseInt(str);
+
+                    if (num < 10 || num > 30) {
+                        msg = "입력한 값이 10 ~ 30을 벗어났습니다.";
+                    } else if (num == rnd) {
+                        msg = cnt + "번째 맞추셨습니다.";
+                    } else if (num > rnd) {
+                        msg = num + "보다 작은 값입니다.";
+                    } else {
+                        msg = num + "보다 큰 값입니다.";
+                    }
+
+                    tv.setText(msg);
+                }
+
+                // 3. 횟수 카운팅!
+                cnt++;
+
+            }
+        });
+
+        Button reset = findViewById(R.id.button_reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 1. 임의의 수 발생
+                rnd = getRandomRange(10, 30);
+
+                // 2. 판정결과 텍스트 클리어
+                tv.setText("");
+
+                cnt = 0;
             }
         });
 
     }
+
+
+    private int getRandomRange(int start, int end) {
+
+
+        return (int) (Math.random() * 20 + 10);
+    }
+
 }
