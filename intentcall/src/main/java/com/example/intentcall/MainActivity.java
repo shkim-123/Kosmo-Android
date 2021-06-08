4e3a3805dc2d46f285e852161802275aece4c477
SH Kim <82145134+shkim-123@users.noreply.github.com>
2021-06-08 17:59:57 +0900

    210608 intentcall

--- intentcall/src/main/java/com/example/intentcall/MainActivity.java
+++ intentcall/src/main/java/com/example/intentcall/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.intentcall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     public void onHomepage(View v) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://shkim-123.github.io"));
@@ -34,4 +36,26 @@ public class MainActivity extends AppCompatActivity {
     public void onFinish(View v) {
         finish();
     }
+
+    public void onRed(View v) {
+        Button btn_reset = findViewById(R.id.button7);
+        LinearLayout layout = findViewById(R.id.my_linearLayout);
+        layout.setBackgroundColor(Color.parseColor("#b71c1c"));
+        btn_reset.setVisibility(View.VISIBLE);
+    }
+
+    public void onBlue(View v) {
+        Button btn_reset = findViewById(R.id.button7);
+        LinearLayout layout = findViewById(R.id.my_linearLayout);
+        layout.setBackgroundColor(Color.parseColor("#0d47a1"));
+        btn_reset.setVisibility(View.VISIBLE);
+    }
+
+    public void onReset(View v) {
+        Button btn_reset = findViewById(R.id.button7);
+        LinearLayout layout = findViewById(R.id.my_linearLayout);
+        layout.setBackgroundColor(Color.parseColor("#FFFFEE"));
+        btn_reset.setVisibility(View.GONE);
+    }
+
 }
